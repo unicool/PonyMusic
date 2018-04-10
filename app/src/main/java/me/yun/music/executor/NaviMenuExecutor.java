43f95d51c354a7dc891c4525578ab2602bec7e88
cfzt\cjf <jianfeng.cheng@zingfon.com>
2018-04-11 00:20:43 +0800

    更改包名

---
+++ app/src/main/java/me/yun/music/executor/NaviMenuExecutor.java
@@ -0,0 +1,78 @@
+package me.yun.music.executor;
+
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.view.MenuItem;
+
+import me.wcy.music.R;
+import me.yun.music.activity.AboutActivity;
+import me.yun.music.activity.MusicActivity;
+import me.yun.music.activity.SettingActivity;
+import me.yun.music.constants.Actions;
+import me.yun.music.service.PlayService;
+import me.yun.music.service.QuitTimer;
+import me.yun.music.storage.preference.Preferences;
+import me.yun.music.utils.ToastUtils;
+
+/**
+ * 导航菜单执行器
+ * Created by hzwangchenyan on 2016/1/14.
+ */
+public class NaviMenuExecutor {
+    private MusicActivity activity;
+
+    public NaviMenuExecutor(MusicActivity activity) {
+        this.activity = activity;
+    }
+
+    public boolean onNavigationItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_setting:
+                startActivity(SettingActivity.class);
+                return true;
+            case R.id.action_night:
+                nightMode();
+                break;
+            case R.id.action_timer:
+                timerDialog();
+                return true;
+            case R.id.action_exit:
+                activity.finish();
+                PlayService.startCommand(activity, Actions.ACTION_STOP);
+                return true;
+            case R.id.action_about:
+                startActivity(AboutActivity.class);
+                return true;
+        }
+        return false;
+    }
+
+    private void startActivity(Class<?> cls) {
+        Intent intent = new Intent(activity, cls);
+        activity.startActivity(intent);
+    }
+
+    private void nightMode() {
+        Preferences.saveNightMode(!Preferences.isNightMode());
+        activity.recreate();
+    }
+
+    private void timerDialog() {
+        new AlertDialog.Builder(activity)
+                .setTitle(R.string.menu_timer)
+                .setItems(activity.getResources().getStringArray(R.array.timer_text), (dialog, which) -> {
+                    int[] times = activity.getResources().getIntArray(R.array.timer_int);
+                    startTimer(times[which]);
+                })
+                .show();
+    }
+
+    private void startTimer(int minute) {
+        QuitTimer.get().start(minute * 60 * 1000);
+        if (minute > 0) {
+            ToastUtils.show(activity.getString(R.string.timer_set, String.valueOf(minute)));
+        } else {
+            ToastUtils.show(R.string.timer_cancel);
+        }
+    }
+}
